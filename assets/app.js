3ba2c0ed7869a0d33f8917c1869ef0b2b53ed204
devNicolasNmk <devnicolasnmk@gmail.com>
2019-07-20 16:17:36 +0200

    modification css js

---
+++ assets/app.js
@@ -0,0 +1,42 @@
+//recuperation des variable
+    
+    var search = document.getElementById("nav-search");
+    var inputSearch = document.getElementById('input-search');
+
+
+    var navjs = document.getElementById("nav-js");
+    var navToShow = document.getElementById("menuJs");
+
+// adeventlistener
+    search.addEventListener("click", openSearch);
+    navjs.addEventListener("click", openMenu);
+
+
+// ------------------ Function ------------------ //
+
+/**
+ *  display or hide input search in header
+ * 
+ */
+function openSearch(){
+    if ( inputSearch.style.display == 'none' ) {
+        inputSearch.style.display='block';
+      
+    } else {
+        inputSearch.style.display='none';
+    }  
+}
+
+/**
+ * 
+ * display or hide menu in nav header
+ * 
+ */
+function openMenu(){
+    if ( navToShow.style.display == 'none' ){
+        navToShow.style.display = 'block' ;
+    } else { 
+        navToShow.style.display='none';
+    }
+}
+
